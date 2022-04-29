5b73efa499768edb483e995b6b6dd2b18954b30e
Jose Jovian <josejovian55@gmail.com>
2022-04-29 10:30:50 +0900

    feat: created modal component

---
+++ components/generic/Modal.js
@@ -0,0 +1,45 @@
+import clsx from "clsx";
+import { createContext, useContext } from "react";
+
+export const ModalContext = createContext(null);
+
+const Modal = () => {
+	const { modal, setModal } = useContext(ModalContext);
+
+	function clearModal() {
+		setModal(null);
+	}
+
+	return (
+		<div
+			className={clsx(
+				modal && [
+					"fixed top-0 left-0 w-screen h-screen pt-8",
+					"flex flex-row justify-center z-40",
+				]
+			)}
+		>
+			<div
+				className={clsx(
+					modal && [
+						"fixed top-0 left-0 w-screen h-screen",
+						"bg-zinc-900 bg-opacity-75",
+					]
+				)}
+				onMouseDown={() => clearModal()}
+			></div>
+			<div
+				className={clsx(
+					modal && [
+						"flex flex-row justify-center p-8 h-min",
+						"text-black bg-slate-100 z-50",
+					]
+				)}
+			>
+				{modal ?? <></>}
+			</div>
+		</div>
+	);
+};
+
+export default Modal;
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import Footer from "../components/Footer";
+import Modal, { ModalContext } from "../components/generic/Modal";
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 
+
 function MyApp({ Component, pageProps }) {
+
+	const [modal, setModal] = useState(null);
+
 	return (
-		<>
+		<ModalContext.Provider value={{
+			modal: modal,
+			setModal: setModal,
+		}}>
+			<Modal />
 			<Navbar />
 			<Component {...pageProps} />
 			<Footer />
-		</>
+		</ModalContext.Provider>
 	);
 }
 
