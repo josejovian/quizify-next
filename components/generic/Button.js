528320e457c2976f2ca92f57def7ad38111337e6
Jose Jovian <josejovian55@gmail.com>
2022-05-06 20:41:21 +0900

    feat: add color and classname props for button

--- components/generic/Button.js
+++ components/generic/Button.js
@@ -6,22 +6,25 @@ const Button = ({
 	variant = "solid",
 	onClick,
 	isDisabled = false,
+	className,
+	color = "blue",
 	type,
 }) => {
 	return (
 		<button
 			id={id}
 			className={clsx(
-				"mt-2 px-4 py-2 shadow-md",
+				"px-4 py-2 shadow-md",
 				"transition-colors duration-100",
 				"disabled:cursor-not-allowed disabled:bg-opacity-50",
 				[
 					variant === "solid" && [
-						"bg-slate-600 text-white",
-						"hover:bg-slate-700",
-						"active:bg-slate-800",
+						`bg-${color}-600 text-white`,
+						`hover:bg-${color}-700`,
+						`active:bg-${color}-800`,
 					],
-				]
+				],
+				className
 			)}
 			onClick={onClick}
 			type={type}
