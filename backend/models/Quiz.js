b3b8b93354b610ed4fec98846f063215338612bf
Jose Jovian <josejovian55@gmail.com>
2022-05-06 20:40:17 +0900

    chore: added author column to quiz

--- backend/models/Quiz.js
+++ backend/models/Quiz.js
@@ -3,8 +3,12 @@ const mongoose = require("mongoose");
 const quizSchema = new mongoose.Schema({
 	name: String,
 	desc: String,
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	},
 	duration: Number,
-});
+}, { timestamps: true });
 
 module.exports = {
 	schema: quizSchema,
