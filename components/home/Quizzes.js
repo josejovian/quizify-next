d5c2226754a7241031ee1c26b17b377e38b92a5d
Jose Jovian <josejovian55@gmail.com>
2022-05-08 23:27:02 +0900

    feat: add edit and delete button for quiz cards

--- components/home/Quizzes.js
+++ components/home/Quizzes.js
@@ -1,16 +1,22 @@
 import clsx from "clsx";
+import Link from "next/link";
 import { MdPerson, MdAccessTimeFilled, MdQuestionAnswer } from "react-icons/md";
+import { connect } from "react-redux";
 import Button from "../generic/Button";
+import { mapDispatchToProps, mapStateToProps } from "../redux/setter";
 
 const Props = ({ children }) => {
 	return (
 		<div className="flex flex-row items-center w-max subtle mr-4">
-			{ children }
+			{children}
 		</div>
 	);
 };
+const Quiz = ({ quiz, isOwner }) => {
+	const bigScreenStyle = "hidden md:flex",
+		smallScreenStyle = "flex md:hidden";
+	const quizSpecialActionStyle = "mt-2 sm:ml-2 sm:mt-0";
 
-const Quiz = ({ quiz }) => {
 	return (
 		<div className="p-4 bg-gray-100 rounded-sm hover:shadow-md hover:bg-gray-200 transition-colors">
 			<h2>{quiz.name}</h2>
@@ -20,26 +26,60 @@ const Quiz = ({ quiz }) => {
 				<div className="flex overflow-x-auto">
 					<Props>
 						<MdPerson className="mr-2" />
-						<span>{ quiz.author.username }</span>
+						<span>{quiz.author.username}</span>
 					</Props>
 					<Props>
 						<MdQuestionAnswer className="mr-2" />
-						<span>{ quiz.questions.length }&nbsp;questions</span>
+						<span className={bigScreenStyle}>
+							{quiz.questions.length}&nbsp;questions
+						</span>
+						<span className={smallScreenStyle}>
+							{quiz.questions.length}&nbsp;q
+						</span>
 					</Props>
 					<Props>
 						<MdAccessTimeFilled className="mr-2" />
-						<span>{ quiz.duration }&nbsp;minutes</span>
+						<span className={bigScreenStyle}>
+							{quiz.duration}&nbsp;minutes
+						</span>
+						<span className={smallScreenStyle}>
+							{quiz.duration}&nbsp;min
+						</span>
 					</Props>
 				</div>
-				<Button className="mt-4 md:mt-0">Do Quiz</Button>
+				<div className="flex flex-col sm:flex-row mt-4 md:mt-0">
+					<Button>Solve</Button>
+					{isOwner && (
+						<>
+							<Button
+								className={quizSpecialActionStyle}
+								variant="secondary"
+							>
+								Edit
+							</Button>
+							<Button
+								className={quizSpecialActionStyle}
+								variant="danger"
+							>
+								Delete
+							</Button>
+						</>
+					)}
+				</div>
 			</div>
 		</div>
 	);
 };
 
-const Quizzes = ({ quizzes }) => {
+const Quizzes = ({ quizzes, loggedIn }) => {
 	const quizElements = quizzes.map((quiz) => {
-		return <Quiz key={quiz._id} quiz={quiz} />;
+		return (
+			<Quiz
+				key={quiz._id}
+				quiz={quiz}
+				isOwner={quiz.author.username === loggedIn.username}
+			/>
+		);
 	});
 
 	return (
@@ -48,7 +88,7 @@ const Quizzes = ({ quizzes }) => {
 			<div
 				className={clsx(
 					"w-screen px-8 lg:px-32 py-16",
-					"grid gap-4 grid-cols-1 xl:grid-cols-2"
+					"grid gap-4 grid-cols-1 2xl:grid-cols-2"
 				)}
 			>
 				{quizElements}
@@ -57,4 +97,4 @@ const Quizzes = ({ quizzes }) => {
 	);
 };
 
-export default Quizzes;
+export default connect(mapStateToProps, mapDispatchToProps)(Quizzes);
